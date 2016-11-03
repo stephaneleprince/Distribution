d3676f9161731ff78c6b75f013005ade20c54aa7
ngodfraind <nicolas.godfraind@gmail.com>
2016-11-03 14:57:05 +0100

    scorm (wip)

---
+++ plugin/scorm/Resources/modules/_old/scorm12.js
@@ -0,0 +1,21 @@
+import $ from 'jquery'
+
+$('.check-status-btn').on('click', function () {
+  var scoTitle = $(this).data('sco-title')
+  var status = $(this).data('best-lesson-status')
+  var totalTime = $(this).data('total-time')
+  var score = $(this).data('bestScore')
+
+  if (score === undefined) {
+    $('#score-tracking-title').addClass('hidden')
+    $('#score-tracking-display').addClass('hidden')
+  } else {
+    $('#score-tracking-title').removeClass('hidden')
+    $('#score-tracking-display').removeClass('hidden')
+    $('#score-tracking-display').html(score)
+  }
+  $('#total-time-tracking-display').html(totalTime)
+  $('#status-tracking-display').html(status)
+  $('#scorm-tracking-modal-title').html(scoTitle)
+  $('#scorm-tracking-modal-box').modal('show')
+})
