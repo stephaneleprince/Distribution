985a9fb381a0e591bfc74aaac32e1721d0d9d817
ngodfraind <nicolas.godfraind@gmail.com>
2016-11-15 17:21:08 +0100

    path

---
+++ plugin/path/Resources/modules/_js/Widget/config.js
@@ -0,0 +1,19 @@
+import $ from 'jquery'
+
+/* global TagTypeAhead */
+
+var allTags = window.allTags
+new TagTypeAhead({
+  tags: allTags,
+  selector: '#innova_path_widget_config_tags'
+})
+// Remove Tags
+$('.removeTag').on('click', function (e) {
+    // Flag the Tag has "to remove"
+  var id = $(this).data('tag-id')
+  $('.tagList').append(`<input type="hidden" name="${window.formName}[removeTags][]" value="` + id + '" />')
+
+    // Remove the displayed label
+  $(this).parents('.label').remove()
+  e.preventDefault()
+})
