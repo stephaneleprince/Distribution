fe0eaf7eea0d2e47e2f995f745249c6e68da2a27
ngodfraind <nicolas.godfraind@gmail.com>
2018-02-19 17:37:30 +0100

    new app folder

---
+++ main/app/ClarolineAppBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\AppBundle;
+
+class ClarolineAppBundle extends Bundle
+{
+}
